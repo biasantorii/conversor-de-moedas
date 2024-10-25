dd7b23f653d951685e975d5a3837ae30f91e31b2
biasantorii <s.beatriz6@alunoescolassociais.org.br>
2024-10-25 08:57:44 -0300

    finalizando o java

---
+++ script.js
@@ -0,0 +1,26 @@
+// Projeto 2A
+document.getElementById('currency-form').addEventListener('submit', function(event){
+    event.preventDefault();
+ 
+    // Obter valores de entrada informados pelo usuário
+    const valor = (document.getElementById('value').value);
+    const daMoeda = document.getElementById('daMoeda').value;
+    const paraMoeda = document.getElementById('paraMoeda').value;
+ 
+    // Definir taxas de câmbio fixas
+    const exchangeRates = {
+        USD: { BRL:  5.70, EUR: 0.93 },
+        BRL: { USD: 0.18, EUR: 0.16 },
+        EUR: { USD: 1.08, BRL: 6.16 }
+    };
+    // Conversão símples de moedas
+    let valorConvertido;
+    if(daMoeda === paraMoeda){
+        valorConvertido = valor;
+    }else{
+        valorConvertido = valor * exchangeRates [daMoeda][paraMoeda];
+    }
+ 
+    const conversao = document.getElementById('conversao');
+    conversao.textContent = `Resultado: ${valorConvertido.toFixed(2)} ${paraMoeda}`;
+});
